b23f797667fda10c61cb8492cdbadaf88e6cdfa2
Faustino Ruiz <ftr1989@gmail.com>
2024-06-14 21:52:56 -0700

    1

--- pascal.py
+++ pascal.py
@@ -1,1 +1,8 @@
-# write a python function....
+# write a python function to find the maximum of three numbers:
+def max_num(a, b, c):
+    return max(a, b, c)
+
+# Test
+print(max_num(3, 6, -5))  # Output: 6
+
+#
